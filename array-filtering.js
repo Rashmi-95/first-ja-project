8437f61e67dedf21c704d43942dbd2fd47745b19
rashmi-95 <rashmi05rash@gmail.com>
2017-01-19 14:26:32 +0530

    -A

---
+++ array-filtering.js
@@ -0,0 +1,5 @@
+var listOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var filteredNumbers = listOfNumbers.filter(function evenNumbers (number) {
+    return number % 2 == 0;
+});
+console.log(filteredNumbers);
